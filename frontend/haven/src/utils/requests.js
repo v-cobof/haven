14fa3ba7e1b7d2fb737ecd274cf6561ce95f27b9
Victor Cobo <v.cobof@outlook.com>
2022-05-09 20:52:01 -0300

    mudada url da api

--- frontend/haven/src/utils/requests.js
+++ frontend/haven/src/utils/requests.js
@@ -1,4 +1,3 @@
-//export const BASE_URL = "http://api-haven.herokuapp.com/api";
+export const BASE_URL = "http://api-haven.herokuapp.com/api";
 
-export const BASE_URL = "https://localhost:44349/api";
-export const CEP_URL = "https://viacep.com.br/ws/SP"
+//export const BASE_URL = "https://localhost:44349/api";
